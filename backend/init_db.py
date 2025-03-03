fb8a58fce53ea685cc198b0f3d77eeb870c49479
Arthaud <arthaudmorin@gmail.com>
2025-03-02 21:39:44 -0500

    new version

--- backend/init_db.py
+++
@@ -1,6 +0,0 @@
-from app.database import Base, engine
-
-# Créer toutes les tables dans la base de données
-print("Création des tables en base de données...")
-Base.metadata.create_all(engine)
-print("Base de données initialisée avec succès !")
